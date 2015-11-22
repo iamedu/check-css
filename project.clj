cdfebd8c5268ee1aa44b53ce4aa13f0a90225d87
Eduardo Díaz <iamedu@gmail.com>
2015-11-22 17:48:02 -0600

    Adding phantomjs driver

--- project.clj
+++ project.clj
@@ -7,6 +7,10 @@
                  [org.clojure/tools.cli "0.3.3"]
                  [clj-webdriver "0.7.2"]
                  [org.seleniumhq.selenium/selenium-java "2.47.1"]
+                 [com.codeborne/phantomjsdriver "1.2.1"
+                                                :exclusion [org.seleniumhq.selenium/selenium-java
+                                                            org.seleniumhq.selenium/selenium-server
+                                                            org.seleniumhq.selenium/selenium-remote-driver]]
                  [org.clojure/core.async "0.2.374"]]
   :main ^:skip-aot check-css.core
   :target-path "target/%s"
